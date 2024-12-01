e3a8d31dec53d2d7b414e703e082111db5ebb21d
Udayachandran-APU <udayachandran.sivakumar23_ug@apu.edu.in>
2024-12-01 20:18:27 +0530

    Update lj_numpy.py with comments for submission

--- Statistical/lj_numpy.py
+++ Statistical/lj_numpy.py
@@ -1,26 +1,25 @@
 import csv, os
 import numpy as np
 import matplotlib.pyplot as plt
-from numba import jit
 
 
-box_size = 450 #volume
+box_size = 450 #volume  <- You might have to reduce to like 250 for smaller number of particles so they converge faster
 ep = 1 #minimum pottential in u-units (uday-units)
 sig = 100 #distance in u-units where pottential is zero
 offset_limit = 5 #step size
 
 def spawn_particles(n):
-    particles = np.random.uniform(0,box_size, size=(n,3)) #I'm using uniform here 
+    particles = np.random.uniform(0,box_size, size=(n,3)) #I'm using uniform here cause it supposidly spawns then unfiromly 
     particles = particles.astype(np.float64)
     return particles
 
 
-def random_offset(particles, iteration, total_iterations): #The mask part hasn't been accounted for
+def random_offset(particles, iteration, total_iterations):
     particles_copy = np.copy(particles)
     shape = particles.shape
     offset_amt = max(0.5, offset_limit * np.exp(- iteration / (0.2* total_iterations)))
     offset_particles =  particles_copy + np.random.uniform(-offset_amt, offset_amt, size=shape)
-    offset_particles = offset_particles.clip(0, box_size) # no bounce back as hard to code
+    offset_particles = offset_particles.clip(0, box_size) 
     return offset_particles
 
 
@@ -59,8 +58,8 @@ def write_to_csv(pot, pot_o, min_pot):
 
 
 if __name__ == "__main__":
-    particles = spawn_particles(20)
-    total_iterations = 70000
+    particles = spawn_particles(20) # <- change number of particles here
+    total_iterations = 70000 # <- change total iterations here
     T = 2
     min_temp = 0.005
     Cooling_rate = 0.9995
@@ -77,7 +76,7 @@ if __name__ == "__main__":
 
         #Send to GLOBAL minima 
         del_E = pot_o - pot
-        if del_E < 0 or np.random.random() < np.exp(-del_E / T):  # Shit's accepting too many wrong shit during later stages so annoying
+        if del_E < 0 or np.random.random() < np.exp(-del_E / T):  # It's accepting too many wrong accepts during later stages doesn't work well
             particles = offset_particles     
 
         #Store the lowest pottential value with coordinates
@@ -88,11 +87,12 @@ if __name__ == "__main__":
         energies = np.append(energies, pot_o)
         
         print(iteration)
-        T*=Cooling_rate
-        T = max(T, min_temp)
+        T*=Cooling_rate #Annealling stuff
+        T = max(T, min_temp) #Making minimum temps so the annealing code doesn't cool it to very small number
         iteration+=1
     
-    show(min_coords, min_pot, save=True)    
+    show(min_coords, min_pot, save=True) # <- Remove if you have issues with matplotlib and directories
+    print(min_pot)
     plt.plot(energies)
     plt.ylim(min_pot, min_pot + 0.2)
     plt.show()
